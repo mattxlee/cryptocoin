9645af38ab5ae8fd863b09bfdab0757d707c48fe
Matthew Lee <mattxlee@gmail.com>
2017-11-07 18:56:40 +0800

    Unfinished transaction.

--- src/transaction.cc
+++ src/transaction.cc
@@ -1,3 +1,48 @@
 #include "transaction.h"
 
+namespace coin {
 
+namespace tx {
+
+DataValue MakeTxSignature(const ecdsa::Key &key, const DataValue &tx_hash,
+                          int out_index) {
+  // Prepare data to hash.
+  int out_index_n = htonl(out_index);
+  DataValue hash_data(tx_hash.size() + sizeof(out_index_n));
+  memcpy(hash_data.data(), tx_hash.data(), tx_hash.size());
+  memcpy(hash_data.data() + tx_hash.size(), &out_index_n, sizeof(out_index_n));
+
+  // Hash with sha256 algorithm.
+  Hash256Builder hash_builder;
+  hash_builder << hash_data;
+  DataValue hash = hash_builder.FinalValue();
+
+  // Make signature to hash value.
+  DataValue signature;
+  bool succ;
+  std::tie(signature, succ) = key.Sign(hash);
+  if (!succ) {
+    // TODO Something wrong!
+  }
+
+  // Return signature data.
+  return signature;
+}
+
+}  // namespace tx
+
+void Transaction::set_pub_key(const DataValue &pub_key) {
+  pub_key_ = pub_key;
+}
+
+void Transaction::add_tx_in(TxIn in) {
+  vec_txin.emplace_back(std::move(in));
+}
+
+void Transaction::add_tx_out(TxOut out) {
+  vec_txout.emplace_back(std::move(out));
+}
+
+DataValue Transaction::CalcHash() const {}
+
+}  // namespace coin
--- src/transaction.h
+++ src/transaction.h
@@ -5,44 +5,81 @@
 #include <vector>
 
 #include "hash_utils.h"
+#include "key.h"
 
 namespace coin {
 
-class Transaction {
+/// Basic transaction.
+class TransactionBase {
  public:
-  virtual ~Transaction() {}
+  /// New transaction type can inherite from.
+  virtual ~TransactionBase() {}
 
+  /// Transaction type value, starts from 0.
   virtual int get_type() const = 0;
 
-  void set_index(int index) { index_ = index; }
-  int get_index() const { return index_; }
-
- private:
-  int index_ = -1;
+  /// Calculate hash value.
+  virtual DataValue CalcHash() const = 0;
 };
 
+/// Transaction incoming tx.
 struct TxIn {
-  DataValue block_hash;
-  int index;
+  DataValue tx_hash;    // From transaction hash value.
+  int out_index;        // txout index.
+  DataValue signature;  // Signature of hash(tx_hash + out_index).
 };
 
+/// Transaction outcoming tx.
 struct TxOut {
-  std::string address;
-  uint64_t value;
+  std::string address;  // To address.
+  uint64_t value;       // Value.
 };
 
-class TransactionSpend : public Transaction {
- public:
-  virtual int get_type() const override { return 0; }
-  void set_pub_key(const std::string &pub_key);
-  void set_signature(const DataValue &signature);
+namespace tx {
 
+/**
+ * @brief Make a signature for tx.
+ *
+ * @param key Private key to make signature.
+ * @param tx_hash Transaction hash value.
+ * @param out_index Out index.
+ *
+ * @return Signature data.
+ */
+DataValue MakeTxSignature(const ecdsa::Key &key, const DataValue &tx_hash,
+                          int out_index);
+
+}  // namespace tx
+
+/// Spend transaction.
+class Transaction : public TransactionBase {
+ public:
+  /// The type of current transaction.
+  virtual int get_type() const override { return 0; }
+
+  /// Set public key, verification of TxIn.
+  void set_pub_key(const DataValue &pub_key);
+
+  /// Add TxIn record.
   void add_tx_in(TxIn in);
+
+  /// Add TxOut record.
   void add_tx_out(TxOut out);
 
+  /// Serialize to stream.
+  template <typename Stream>
+  size_t Serialize(Stream &s) const {
+  }
+
+  /// Unserialize from stream.
+  template <typename Stream>
+  size_t Unserialize(const Stream &s) {}
+
+  /// Calculate hash value.
+  virtual DataValue CalcHash() const override;
+
  private:
-  std::string pub_key_;
-  DataValue signature_;
+  DataValue pub_key_;
   std::vector<TxIn> vec_txin;
   std::vector<TxOut> vec_txout;
 };
