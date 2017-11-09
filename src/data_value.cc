52315072e9d05793853b6cb8434677bc46d9cbcd
Matthew Lee <mattxlee@gmail.com>
2017-11-09 17:00:26 +0800

    Fix compile with `duplicated symbols` error.

--- src/data_value.cc
+++ src/data_value.cc
@@ -7,7 +7,15 @@ namespace data {
 
 namespace utils {
 
-// EMPTY.
+uint64_t HostToNet(uint64_t value) {
+  uint64_t h = (value & 0xffffffff00000000) >> 32;
+  uint64_t l = value & 0x00000000ffffffff;
+  h = htonl(h);
+  l = ntohl(l);
+  return (l << 32) + h;
+}
+
+uint64_t NetToHost(uint64_t value_n) { return HostToNet(value_n); }
 
 }  // namespace utils
 
--- src/data_value.h
+++ src/data_value.h
@@ -50,15 +50,9 @@ IntType NetToHost(IntType value_n) {
   return value;
 }
 
-uint64_t HostToNet(uint64_t value) {
-  uint64_t h = (value & 0xffffffff00000000) >> 32;
-  uint64_t l = value & 0x00000000ffffffff;
-  h = htonl(h);
-  l = ntohl(l);
-  return (l << 32) + h;
-}
+uint64_t HostToNet(uint64_t value);
 
-uint64_t NetToHost(uint64_t value_n) { return HostToNet(value_n); }
+uint64_t NetToHost(uint64_t value_n);
 
 }  // namespace utils
 
