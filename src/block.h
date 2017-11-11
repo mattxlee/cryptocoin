53d933ddf51708e63e057662f890b87bf5857216
Matthew Lee <mattxlee@gmail.com>
2017-11-11 19:58:42 +0800

    Add properties for `Transaction`.

--- src/block.h
+++ src/block.h
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "data_value.h"
+#include "transaction.h"
 
 namespace coin {
 
@@ -18,12 +19,14 @@ class Block {
  private:
   // Block base info.
   data::Buffer block_hash_;
-  int version_ = 0x01000000;
+  int version_ = 1;
   time_t timestamp_;
+  uint32_t height_;
   // Below values contain in the calculation of block hash.
   data::Buffer prev_hash_;
   data::Buffer merkl_root_hash_;
   data::Buffer nonce_;
+  std::vector<Transaction> trans_;
 };
 
 }  // namespace coin
