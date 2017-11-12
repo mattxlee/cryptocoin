1ebd0247f587f18a89184d102aa9a86c23ba52fb
Matthew Lee <mattxlee@gmail.com>
2017-11-12 21:50:44 +0800

    Serialize/Unserialize block.

--- src/block.cc
+++ src/block.cc
@@ -1,5 +1,46 @@
 #include "block.h"
 
 namespace coin {
+namespace blk {
+
+void Block::set_block_hash(const bn::HashNum &num) { block_hash_ = num; }
+
+const bn::HashNum &Block::get_block_hash() const { return block_hash_; }
+
+void Block::set_timestamp(time_t timestamp) { timestamp_ = timestamp; }
+
+time_t Block::get_timestamp() const { return timestamp_; }
+
+void Block::set_height(uint32_t height) { height_ = height; }
+
+uint32_t Block::get_height() const { return height_; }
+
+void Block::set_prev_hash(const bn::HashNum &num) { prev_hash_ = num; }
+
+const bn::HashNum &Block::get_prev_hash() const { return prev_hash_; }
+
+void Block::set_merkle_root_hash(const bn::HashNum &hash) {
+  merkle_root_hash_ = hash;
+}
+
+const bn::HashNum &Block::get_merkle_root_hash() const {
+  return merkle_root_hash_;
+}
+
+void Block::set_nonce(uint32_t nonce) { nonce_ = nonce; }
+
+uint32_t Block::get_nonce() const { return nonce_; }
+
+void Block::set_difficult(const bn::HashNum &difficult) {
+  difficult_ = difficult;
+}
+
+const bn::HashNum &Block::get_difficult() const { return difficult_; }
+
+std::vector<Transaction> &Block::get_trans() { return trans_; }
+
+const std::vector<Transaction> &Block::get_trans() const { return trans_; }
+
+}  // namespace blk
 
 }  // namespace coin
--- src/block.h
+++ src/block.h
@@ -16,22 +16,66 @@ namespace blk {
 class Block {
  public:
   template <typename Stream>
-  void Serialize(Stream &s) {}
+  void Serialize(Stream &s) {
+    data::MakeValue(version_).WriteToStream(s);
+    data::MakeValue(timestamp_).WriteToStream(s);
+    data::MakeValue(height_).WriteToStream(s);
+    data::MakeValue(block_hash_).WriteToStream(s);
+    data::MakeValue(prev_hash_).WriteToStream(s);
+    data::MakeValue(merkle_root_hash_).WriteToStream(s);
+    data::MakeValue(nonce_).WriteToStream(s);
+    data::MakeValue(difficult_).WriteToStream(s);
+    data::MakeValue(trans_).WriteToStream(s);
+  }
 
   template <typename Stream>
-  void Unserialize(Stream &s) {}
+  void Unserialize(Stream &s) {
+    data::MakeValue(version_).ReadFromStream(s);
+    data::MakeValue(timestamp_).ReadFromStream(s);
+    data::MakeValue(height_).ReadFromStream(s);
+    data::MakeValue(block_hash_).ReadFromStream(s);
+    data::MakeValue(prev_hash_).ReadFromStream(s);
+    data::MakeValue(merkle_root_hash_).ReadFromStream(s);
+    data::MakeValue(nonce_).ReadFromStream(s);
+    data::MakeValue(difficult_).ReadFromStream(s);
+    data::MakeValue(trans_).ReadFromStream(s);
+  }
+
+  void set_block_hash(const bn::HashNum &num);
+  const bn::HashNum &get_block_hash() const;
+
+  void set_timestamp(time_t timestamp);
+  time_t get_timestamp() const;
+
+  void set_height(uint32_t height);
+  uint32_t get_height() const;
+
+  void set_prev_hash(const bn::HashNum &num);
+  const bn::HashNum &get_prev_hash() const;
+
+  void set_merkle_root_hash(const bn::HashNum &hash);
+  const bn::HashNum &get_merkle_root_hash() const;
+
+  void set_nonce(uint32_t nonce);
+  uint32_t get_nonce() const;
+
+  void set_difficult(const bn::HashNum &difficult);
+  const bn::HashNum &get_difficult() const;
+
+  std::vector<Transaction> &get_trans();
+  const std::vector<Transaction> &get_trans() const;
 
  private:
   // Block base info.
-  bn::HashNum block_hash_;
   int version_ = 1;
   time_t timestamp_ = 0;
   uint32_t height_ = -1;
+  bn::HashNum block_hash_;
   // Below values contain in the calculation of block hash.
   bn::HashNum prev_hash_;
   bn::HashNum merkle_root_hash_;
   uint32_t nonce_ = 0;
-  bn::HashNum diff_;
+  bn::HashNum difficult_;
   std::vector<Transaction> trans_;
 };
 
