24fb91d2eeaa98a147b02dc81f2fbdcaf9e3e794
Matthew Lee <mattxlee@gmail.com>
2017-11-13 21:42:21 +0800

    Add basic info. for genesis transaction.

--- src/block_builder.cc
+++ src/block_builder.cc
@@ -9,12 +9,21 @@ namespace blk {
 
 Block BlockBuilder::BuildGenesisBlock() {
   Block block;
+  // Initialize block basic data.
   block.set_timestamp(time(NULL));
   block.set_height(0);
   block.set_block_hash(ZERO_HASH256);
   block.set_prev_hash(ZERO_HASH256);
   block.set_merkle_root_hash(ZERO_HASH256);
   block.set_difficult(ZERO_HASH256);
+  // Add first transaction.
+  Transaction coin_base_trans;
+  coin::TxOut tx_out;
+  tx_out.address = "12iPmmNQQ9oqnjT5Nj7eg7bPmQtLXUQUmq";
+  tx_out.amount = 1000;
+  coin_base_trans.add_tx_out(tx_out);
+  block.get_trans().push_back(coin_base_trans);
+  // Returns block.
   return block;
 }
 
